77b251146030a3431b01e0034750caea51243792
Martin <martin@noelwapp.nl>
2019-01-15 21:25:45 +0100

    better building of typescripts with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,29 @@ const pump = require('pump');
 const rename = require("gulp-rename");
 const connect = require('gulp-connect');
 const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
 
 
-gulp.task('scripts', function(done) {
-    gulp.src("src/**/*.ts")
+gulp.task('clean', function (done) {
+    del(['target/*']);
+    done();
+});
+
+gulp.task('copy-source',function (done) {
+    return gulp.src("src/**/*.ts")
         .pipe(gulp.dest('target'));
+});
 
-    gulp.src("target/**/*.ts") // or tsProject.src()
+gulp.task('scripts', function (done) {
+
+    return gulp.src("target/**/*.ts") // or tsProject.src()
         .pipe(sourcemaps.init())
         .pipe(tsProject()).js
         .pipe(sourcemaps.write({includeContent: false}))
-        .pipe(gulp.dest('target'));
+    .pipe(gulp.dest('target'));
+});
 
+gulp.task('dependencies', function (done) {
     gulp.src("node_modules/table-dragger/dist/**/*.js") // or tsProject.src()
         .pipe(gulp.dest('target'));
 
@@ -28,26 +39,38 @@ gulp.task('scripts', function(done) {
     gulp.src("node_modules/flatpickr/dist/flatpickr.min.js")
         .pipe(gulp.dest('target'));
 
+    done();
+});
+
+gulp.task('to-test', function (done) {
+
     gulp.src("target/*.js")
         .pipe(gulp.dest('test/lib'));
 
     gulp.src("target/*.ts")
         .pipe(gulp.dest('test/lib'));
+
     done();
 });
 
 gulp.task('ugly', function (cb) {
-    pump([
+    return pump([
             gulp.src('target/*.js'),
             uglify(),
-            rename({ suffix: '.min' }),
-            gulp.dest('dist')
+            rename({suffix: '.min'}),
+            gulp.dest('target')
         ],
         cb
     );
 });
 
-gulp.task('connect', function() {
+gulp.task('dist', function (cb) {
+    gulp.src("target/*.min.js")
+        .pipe(gulp.dest('dist'));
+    cb();
+});
+
+gulp.task('connect', function () {
     connect.server({
         root: './test/',
         livereload: true
@@ -55,11 +78,12 @@ gulp.task('connect', function() {
 });
 
 
-gulp.task('watch', function() {
-    gulp.watch('src/**/*.ts', gulp.series('scripts'));
-    //gulp.watch('src/**/*.ts', gulp.series('ugly'));
+
+gulp.task('build', gulp.series(['clean', 'copy-source', 'scripts','ugly', 'dependencies', 'to-test']));
+
+gulp.task('watch', function () {
+    gulp.watch('src/**/*.ts', gulp.series('build'));
+    gulp.watch('test/index.html', gulp.series('build'));
 });
 
-
-//gulp.task('default', gulp.series(['scripts', 'ugly']));
 gulp.task('server', gulp.parallel(['connect', 'watch']));
