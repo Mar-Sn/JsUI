a515bfc0a68e1dabcacef490e671dd3c7e8465fd
Martin <martin@noelwapp.nl>
2019-02-01 14:30:27 +0100

    fully extend panel as Div

--- src/panel.ts
+++ src/panel.ts
@@ -1,41 +1,26 @@
 // @ts-ignore
 import {Component} from "Component";
-// @ts-ignore
-import {Random} from "Random";
 
 // @ts-ignore
 import {Div} from "Div";
 
-export class Panel extends Div{
+export class Panel extends Div {
     readonly title: string;
-    private readonly panel: Div;
 
     constructor(title: string, classes: string | string[]) {
-        super();
-        this.title = title;
-
-        let mergedClasses = [];
-        mergedClasses.push("panel");
-        if(typeof classes === "string"){
-            this.panel.mergedClasses.push(classes);
-        }else if(typeof classes === "object"){
-            classes.forEach(function(item){
-                // @ts-ignore
-                mergedClasses.push(item);
-            });
+        let _classes = ['panel'];
+        if (typeof classes === "string") {
+            _classes.push(classes);
+        } else if (typeof classes === "object") {
+            _classes.concat(classes);
         }
 
-        this.panel = new Div(mergedClasses);
-
-
-        // @ts-ignore
-        super.children().forEach(child =>{
-            this.panel.appendChild(child.getElement());
-        });
+        super(_classes);
+        this.title = title;
     }
 
     getElement(): HTMLElement | null {
-        return this.panel.getElement();
+        return super.getElement();
     }
 
     /**
@@ -45,8 +30,11 @@ export class Panel extends Div{
     set(component: Component) {
         super.clearChildren();
         super.addChild(component);
-        this.panel.innerHTML = "";
-        this.panel.addElement(component);
+        let element = this.getElement();
+        if(element != null){
+            element.innerHTML = "";
+            super.addElement(component);
+        }
     };
 
     /**
@@ -55,7 +43,10 @@ export class Panel extends Div{
      */
     append(component: Component) {
         super.addChild(component);
-        this.panel.addElement(component);
+        let element = this.getElement();
+        if(element != null) {
+            super.addElement(component);
+        }
     };
 
 }
