f3677758263fa161f8a2098c28846b773c342961
Martin <martin@noelwapp.nl>
2019-02-07 11:04:17 +0100

    first unit test for tableAdapter

---
+++ unit/tableAdapter.spec.ts
@@ -0,0 +1,24 @@
+import {TableData, TableRow} from "../src/table_adapter";
+import {expect} from 'chai';
+
+
+describe("TableAdapter", () => {
+    describe("Remove row", () => {
+
+        it("One row should be correctly added", () => {
+            let tableData = new TableData([new TableRow()]);
+            tableData.addRow(new TableRow());
+            expect(tableData.rows.length).to.equal(2);
+        });
+        it("Multiple rows should be correctly added", () => {
+            let tableData = new TableData([new TableRow()]);
+            tableData.addRows([new TableRow(), new TableRow()]);
+            expect(tableData.rows.length).to.equal(3);
+        });
+        it("Row should be correctly removed", () => {
+            let tableData = new TableData([new TableRow(), new TableRow(), new TableRow(), new TableRow()]);
+            tableData.removeRow(2);
+            expect(tableData.rows.length).to.equal(3);
+        });
+    })
+});
